6041468e0902805643548d2fe0d79cbffd6aff20
zhuoJinYang <956541455@qq.com>
2022-08-08 23:09:19 +0800

    初始化项目,服务端配置mybatisPlus基础结构

---
+++ rSpace-serv/db/src/main/java/com/rspace/db/entity/User.java
@@ -0,0 +1,37 @@
+package com.rspace.db.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zjy
+ * @since 2022-08-08
+ */
+@Getter
+@Setter
+@TableName("sys_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    private String name;
+
+    private String sex;
+
+    private Long fans;
+
+    private String introduce;
+
+
+}
---
+++ rSpace-serv/db/src/main/java/com/rspace/db/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.rspace.db.mapper;
+
+import com.rspace.db.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zjy
+ * @since 2022-08-08
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
