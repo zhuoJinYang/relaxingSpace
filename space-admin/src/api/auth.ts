ce93cbb73b0ba48eb2b57aa6d0a49793310ad2f5
zhuoJinYang <956541455@qq.com>
2022-09-28 17:03:58 +0800

    Revert "卓儿子"
    This reverts commit 4f822d1a

--- space-admin/src/api/auth.ts
+++ space-admin/src/api/auth.ts
@@ -2,6 +2,10 @@ import {AxiosResult, LoginSuccessData} from "@/types/api";
 import axiosInstance from "@/axois";
 import {LoginAccount} from "@/types/user";
 
+export const getPasswordSecretKey = ():AxiosResult<string> => {
+    return axiosInstance.get('/auth/password-secret-key')
+}
+
 export const apiLogin = (data:LoginAccount):AxiosResult<LoginSuccessData> => {
     return axiosInstance.post('/auth/login',data)
 }
