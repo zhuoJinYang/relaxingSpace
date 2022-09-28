f28740b52656569563632b96e4747d1ac939d8d3
zhuoJinYang <956541455@qq.com>
2022-09-28 23:07:07 +0800

    优化blog主页内容页面缩放(左右拉动时适应布局)

--- space-admin/src/views/modules/forum/MainPage.vue
+++ space-admin/src/views/modules/forum/MainPage.vue
@@ -1,30 +1,37 @@
 <template>
-  <div style="width: 100%;margin-top: 20px;display: flex;padding: 0 20px">
-    <div class="page-left">
-      <Blog v-for="i in 20"/>
-    </div>
-    <div class="page-right">
-      <a-affix>
-        <div class="expand-content">
-          <a-card title="推荐" style="width: 300px">
-            <p>推荐1</p>
-            <p>推荐2</p>
-            <p>推荐3</p>
-          </a-card>
-        </div>
-      </a-affix>
-    </div>
+  <div style="width: 100%;margin-top: 20px;padding: 0 20px">
+    <a-row :gutter="10">
+      <a-col :xs="24" :sm="24" :md="19" :lg="19" :xl="19">
+        <Blog v-for="i in 20"/>
+      </a-col>
+      <a-col :xs="0" :sm="0" :md="5" :lg="5" :xl="5">
+        <a-affix>
+          <div class="expand-content">
+            <a-card title="推荐">
+              <p>推荐1</p>
+              <p>推荐2</p>
+              <p>推荐3</p>
+            </a-card>
+          </div>
+        </a-affix>
+      </a-col>
+    </a-row>
+<!--    <div class="page-left">-->
+<!--      <Blog v-for="i in 20"/>-->
+<!--    </div>-->
+<!--    <div class="page-right">-->
+<!--      <a-affix>-->
+<!--        <div class="expand-content">-->
+<!--          <a-card title="推荐" style="width: 300px">-->
+<!--            <p>推荐1</p>-->
+<!--            <p>推荐2</p>-->
+<!--            <p>推荐3</p>-->
+<!--          </a-card>-->
+<!--        </div>-->
+<!--      </a-affix>-->
+<!--    </div>-->
   </div>
 </template>
-
-<style lang="scss">
-.page-left{
-  width: 80%;
-}
-.expand-content{
-  padding-left: 20px;
-}
-</style>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
