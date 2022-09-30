dc7ccc7d1e1fbd75e95a3f7cbf638bc50541826f
zhuoJinYang <956541455@qq.com>
2022-09-30 11:01:36 +0800

    抄的redis默认配置,保留着以后用

---
+++ space-serv/domain/src/main/java/com/space/domain/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.space.domain.config;
+
+import com.space.domain.util.JacksonUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+//@Configuration
+public class RedisConfig {
+
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        // 将template 泛型设置为 <String, Object>
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory);
+//
+//        //序列化设置
+//        // key、hash的key 采用 String序列化方式
+//        template.setKeySerializer(RedisSerializer.string());
+//        template.setHashKeySerializer(RedisSerializer.string());
+//        // value、hash的value 采用 Jackson 序列化方式
+////        template.setValueSerializer(RedisSerializer.json());
+////        template.setHashValueSerializer(RedisSerializer.json());
+//        Jackson2JsonRedisSerializer<Object> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//        jsonRedisSerializer.setObjectMapper(JacksonUtil.mapper);
+//        template.setValueSerializer(jsonRedisSerializer);
+//        template.setHashValueSerializer(jsonRedisSerializer);
+//        template.afterPropertiesSet();
+//
+//        return template;
+//    }
+}
