bb2e8d14f62763829ba784e9ab36b1ab4970de2b
zhuoJinYang <956541455@qq.com>
2022-10-20 14:31:44 +0800

    修改接口请求地址

--- space-admin/src/api/forum/blog.ts
+++ space-admin/src/api/forum/blog.ts
@@ -4,13 +4,13 @@ import {BlogModel} from "@/types/model";
 
 
 export const apiGetBlogList = ():AxiosResult<BlogSucessData> => {
-    return axiosInstance.get('/blog/list')
+    return axiosInstance.get('/blog')
 }
 
 export const apiSaveBlogList = (data:BlogModel):AxiosResult => {
-    return axiosInstance.post('/blog/save',data)
+    return axiosInstance.post('/blog',data)
 }
 
 export const apiGetBlogDetail = (id:string):AxiosResult<BlogDetailData> => {
-    return axiosInstance.get('/blog/detail',{params:{id}})
+    return axiosInstance.get('/blog/'+id)
 }
